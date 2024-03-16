f3939b4f0dc5f3f90e635c0ddefa96e134c4ec8e
Aral Sen <asen4@binghamton.edu>
2024-03-16 13:29:04 -0400

    feat: click events

---
+++ 7-click-events/src/App.js
@@ -0,0 +1,11 @@
+// click event = An interaction when a user clicks on a specific element. We can respond to clicks by passing a callback
+// function to the onClick prop of the element we want to listen for clicks on.
+
+import Button from "./Button.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
+
+function App() {
+  return(<ProfilePicture/>);
+}
+
+export default App;
