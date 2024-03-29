199b8f85ab2ad8dfe2cb4857f23212477460e74f
Aral Sen <asen4@binghamton.edu>
2024-03-28 21:51:45 -0400

    feat: useEffect() hook

---
+++ 16-use-effect-hook/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from "./MyComponent";
+import MyComponent2 from "./MyComponent2";
+
+
+function App() {
+  return (
+    <MyComponent2/>
+  );
+}
+
+export default App;
